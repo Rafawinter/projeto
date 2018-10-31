40ee5a2266bae802eb51bb4c2e77e6db89b2baf0
Rafawinter <43618578+Rafawinter@users.noreply.github.com>
2018-10-31 09:04:50 -0300

    Add files via upload

---
+++ rest_blog/api/categoria/create.php
@@ -0,0 +1,39 @@
+<?php	
+	header('Content-Type: application/json');
+
+	if(!isset($_SERVER['PHP_AUTH_USER'])){
+		header('WWW-Authenticate: Basic realm="Página Restrita"');
+		header('HTTP/1.0 401 Unauthorized');
+		die(json_encode(["mensagem" => "autenticação necessária"]));
+	}
+	if($_SERVER['PHP_AUTH_USER'] == 'admin' && $_SERVER['PHP_AUTH_PW'] == 'admin'){
+		echo json_encode(["mensagem" => "Bem-vindo"]);
+		exit;
+	}elseif(!($_SERVER['PHP_AUTH_USER'] == 'admin' && $_SERVER['PHP_AUTH_PW'] == 'admin')){
+		header('HTTP/1.0 401 Unauthorized');
+		die(json_encode(["mensagem" => "dados incorretos"]));
+	}
+	
+	require_once '../../config/Conexao.php';
+	require_once '../../models/Categoria.php';
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+
+		$db = new Conexao();
+		$con = $db->getConexao();
+
+		$categoria = new Categoria($con);
+		
+		$dados = json_decode(file_get_contents("php://input"));	
+
+		$categoria->nome = $dados->nome;
+		$categoria->descricao = $dados->descricao;
+
+		if($categoria->create()) {
+			$res = array('mensagem','Categoria criada');
+		} else {
+			$res = array('mensagem','Erro na criação da categoria');
+		}
+		echo json_encode($res);
+	}else{
+		echo json_encode(['mensagem'=> 'método não suportado']);
+	}
